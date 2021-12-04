4d2964f7812ddbdc9a7a3a3b058802c7a9b95ec7
Alex <vuyanalex@gmail.com>
2021-12-04 17:00:14 +0300

    added jwt & login route & login validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
@@ -42,9 +44,52 @@ router.post('/register',
     })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+    '/login',
+    [
+        check('email', 'Wrong email').normailzeEmail().isEmail(),
+        check('password', 'Enter pwd').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
-})
+            if (errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Wrong login data'
+                })
+            }
+
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({ message: 'User is\'t find' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Wrong pwd' });
+            }
+
+            const token = jwt.sign(
+                { userID: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: '1h' }
+            )
+
+            res.json({
+                token, userId: user.id
+            })
+
+        } catch (e) {
+            res.status(500).json({ message: 'Smth wrong, try again' })
+        }
+
+    })
 
 
 module.exports = router;
